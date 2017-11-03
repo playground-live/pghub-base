743fbf689eda5b7020e0d69ce42d3e2ff0df7ba5
ebkn12 <ktennis.mqekr12@gmail.com>
2017-11-03 17:26:12 +0900

    add assign_numbers option to pghub/config

--- lib/pghub/config.rb
+++ lib/pghub/config.rb
@@ -1,10 +1,11 @@
 module Pghub
   class Config
-    attr_accessor :github_organization, :github_access_token
+    attr_accessor :github_organization, :github_access_token, :assign_numbers
 
     def initialize
       @github_organization = ''
       @github_access_token = ''
+      @assign_numbers = {}
     end
   end
 end
