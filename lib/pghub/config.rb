ba538c9bb252dfdfc3edc1b4cde964b6fc41b077
ebkn12 <ktennis.mqekr12@gmail.com>
2017-11-13 16:16:09 +0900

    modify name of num_of_assignees(reviewers) variable

--- lib/pghub/config.rb
+++ lib/pghub/config.rb
@@ -2,14 +2,14 @@ module Pghub
   class Config
     attr_accessor :github_organization,
                   :github_access_token,
-                  :num_of_assignees,
-                  :num_of_reviewers
+                  :num_of_assignees_per_team,
+                  :num_of_reviewers_per_team
 
     def initialize
       @github_organization = ''
       @github_access_token = ''
-      @num_of_assignees = {}
-      @num_of_reviewers = {}
+      @num_of_assignees_per_team = {}
+      @num_of_reviewers_per_team = {}
     end
   end
 end
